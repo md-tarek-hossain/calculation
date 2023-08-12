4cb68bbd184185fc07b600eaad99173da692cf7d
md tarek hossain <hossaintarek944@gmail.com>
2023-08-12 19:24:17 +0600

    adde some values

---
+++ cal.js
@@ -0,0 +1,13 @@
+var jim = 84;
+var dela = 75;
+var cinku =97;
+
+if (jim>dela && jim>cinku){
+    console.log('jim will get the pizza')
+}
+else if(dela>jim && dela> cinku){
+    console.log('dela wil win the Pizza')
+}
+else{
+    console.log('Cinku Will win the Pizza')
+}
---
+++ math.js
@@ -0,0 +1,15 @@
+// let number1 = 3;
+// let number2 = 9;
+// [ number1, number2] = [number2, number1]
+// console.log(number1, number2)
+// console.log(number1, number2);
+
+// let temp = number1;
+//  number1 = number2;
+//  number2 = temp;
+// console.log(number1, number2)
+
+
+var number = 3.6;
+var number1 = Math.round(number);
+console.log(number1);
